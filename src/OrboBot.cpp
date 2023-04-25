ff1399f4a86ea0c6e464ed7478d9d07edc1b46dc
Oleksandr314 <oshev4uk@gmail.com>
2023-04-25 06:27:05 +0300

    implemented PI feedback control system

--- src/OrboBot.cpp
+++ src/OrboBot.cpp
@@ -1,6 +1,23 @@
 #include "OrboBot.h"
 
-
+int Robot::right_encoder_val = 0;
+int Robot::left_encoder_val = 0;
+int Robot::prev_right_encoder_val = 0;
+int Robot::prev_left_encoder_val = 0;
+		
+int Robot::current_right_motor_speed = 0;
+int Robot::current_left_motor_speed = 0;
+		
+int Robot::target_right_motor_speed = 0;
+int Robot::target_left_motor_speed = 0;
+		
+int Robot::speed_error_right_motor = 0;
+int Robot::speed_integral_error_right_motor = 0;
+int Robot::right_motor_PWM = 0;
+		
+int Robot::speed_error_left_motor = 0;
+int Robot::speed_integral_error_left_motor = 0;
+int Robot::left_motor_PWM = 0;
 
 void Robot::init(){
     pinMode(LEFT_MOTOR_DIR_PIN, OUTPUT);
@@ -8,11 +25,11 @@ void Robot::init(){
     pinMode(LEFT_MOTOR_SPEED_PIN, OUTPUT);
     pinMode(RIGHT_MOTOR_SPEED_PIN, OUTPUT);
 
-    attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_CHANNEL_A), left_encoder_ISR, RISING);
-    attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER_CHANNEL_A), right_encoder_ISR, RISING);
+    attachInterrupt(digitalPinToInterrupt(LEFT_ENCODER_CHANNEL_A), Robot::left_encoder_ISR, RISING);
+    attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODER_CHANNEL_A), Robot::right_encoder_ISR, RISING);
     
     Timer1.initialize(100000);         // initialize timer1, and set a 0.1 second period
-    Timer1.attachInterrupt(timer_ISR);  // attaches timer_ISR() as a timer overflow interrupt
+    Timer1.attachInterrupt(Robot::timer_ISR);  // attaches timer_ISR() as a timer overflow interrupt
    
 }  
 
@@ -22,17 +39,64 @@ void Robot::drive(){
      
 }
 
-void Robot::left_encoder_ISR(){
-  
-  }
+int Robot::get_right_encoder_val(){return right_encoder_val;}
+int Robot::get_left_encoder_val(){return left_encoder_val;}
 
-void Robot::right_encoder_ISR(){
-  
-  }  
+static void Robot::right_encoder_ISR(){
+	  if(digitalRead(RIGHT_ENCODER_CHANNEL_B) == HIGH){right_encoder_val++;}
+    else if(digitalRead(RIGHT_ENCODER_CHANNEL_B) == LOW){right_encoder_val--;}
+}  
+
+static void Robot::left_encoder_ISR(){
+    if(digitalRead(LEFT_ENCODER_CHANNEL_B) == HIGH){left_encoder_val--;}
+    else if(digitalRead(LEFT_ENCODER_CHANNEL_B) == LOW){left_encoder_val++;}
+}
 
 
-// calculate PD control by spped 
+
+
 void Robot::timer_ISR(){
+	
+    current_right_motor_speed = right_encoder_val - prev_right_encoder_val;
+    prev_right_encoder_val = right_encoder_val;
+    speed_error_right_motor = target_right_motor_speed - current_right_motor_speed;
+
+
+    speed_integral_error_right_motor += speed_error_right_motor;
+    speed_integral_error_right_motor = constrain(speed_integral_error_right_motor, -255, 255);
+    right_motor_PWM  =  (int)(Kp * speed_error_right_motor  + Ki * speed_integral_error_right_motor);
+    right_motor_PWM = constrain(right_motor_PWM,-255,255);
   
+    analogWrite(RIGHT_MOTOR_SPEED_PIN, abs(right_motor_PWM));
   
+    if(right_motor_PWM <= 0)
+    {
+        digitalWrite(RIGHT_MOTOR_DIR_PIN, LOW); 
+    }
+    if(right_motor_PWM > 0)
+    {
+        digitalWrite(RIGHT_MOTOR_DIR_PIN, HIGH);
+    }
+	
+	  current_left_motor_speed = left_encoder_val - prev_left_encoder_val;
+    prev_left_encoder_val = left_encoder_val;
+    speed_error_left_motor = target_left_motor_speed - current_left_motor_speed;
+
+
+    speed_integral_error_left_motor += speed_error_left_motor;
+    speed_integral_error_left_motor = constrain(speed_integral_error_left_motor, -255, 255);
+    left_motor_PWM  =  (int)(Kp * speed_error_left_motor  + Ki * speed_integral_error_left_motor);
+    left_motor_PWM = constrain(left_motor_PWM,-255,255);
+  
+    analogWrite(LEFT_MOTOR_SPEED_PIN, abs(left_motor_PWM));
+  
+    if(left_motor_PWM <= 0)
+    {
+        digitalWrite(LEFT_MOTOR_DIR_PIN, LOW); 
+    }
+    if(right_motor_PWM > 0)
+    {
+        digitalWrite(LEFT_MOTOR_DIR_PIN, HIGH);
+    }
+	
 }  
--- src/OrboBot.h
+++ src/OrboBot.h
@@ -3,17 +3,24 @@
 #include <Arduino.h>
 #include "TimerOne.h"
 
-const int LEFT_MOTOR_DIR_PIN = 13;
 const int RIGHT_MOTOR_DIR_PIN = 12;
-const int LEFT_MOTOR_SPEED_PIN = 6;
-const int RIGHT_MOTOR_SPEED_PIN = 9;
+const int RIGHT_MOTOR_SPEED_PIN = 6;
 
-const int LEFT_ENCODER_CHANNEL_A = 2;
-const int LEFT_ENCODER_CHANNEL_B = 5;
-const int RIGHT_ENCODER_CHANNEL_A = 3;
-const int RIGHT_ENCODER_CHANNEL_B = 7;
+const int LEFT_MOTOR_DIR_PIN = 13;
+const int LEFT_MOTOR_SPEED_PIN = 11;
+
+
+const int RIGHT_ENCODER_CHANNEL_A = 2;
+const int RIGHT_ENCODER_CHANNEL_B = 5;
+
+const int LEFT_ENCODER_CHANNEL_A = 3;
+const int LEFT_ENCODER_CHANNEL_B = 7;
+
 
 const int BUZZER_PIN = 4;
+
+const float Kp = 2.55;
+const float Ki = 1.0;
 
 
 
@@ -21,7 +28,27 @@ class Robot {
     private:
         int wheel_diameter = 55;
         int axle_track = 100;
-        
+		
+		   static int right_encoder_val;
+       static int left_encoder_val;
+		
+	      static int prev_right_encoder_val;
+        static int prev_left_encoder_val;
+		
+		    static int current_right_motor_speed;
+		    static int current_left_motor_speed;
+		
+		    static int target_right_motor_speed;
+		    static int target_left_motor_speed;
+		
+		    static int speed_error_right_motor;
+		    static int speed_integral_error_right_motor;
+		    static int right_motor_PWM;
+		
+	      static int speed_error_left_motor;
+		    static int speed_integral_error_left_motor;
+		    static int left_motor_PWM;
+		
         static void left_encoder_ISR();
         static void right_encoder_ISR();
         static void timer_ISR();
@@ -31,7 +58,11 @@ class Robot {
         
         void init();
         void drive();
+        int get_right_encoder_val();
+        int get_left_encoder_val();
 
 };
+
+
 
 #endif
